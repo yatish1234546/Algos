287d12dab95dea98de52a031a0446ab4fb3d5f6b
yatish1234546 <katagaryatish@gmail.com>
2020-12-22 23:04:01 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import "./style.css";
 // import "./anagrams/anagrams.js";
 // import "./multiple-pointers/checkForSum.js";0
 // import "./Sorting/BubbleSort.js";
-import "./Search/BinarySearch.js";
+// import "./Search/BinarySearch.js";
+import "./Search/NaiveStringSearch.js";
+
 // Write Javascript code!
 console.log("JS Loaded");
 const appDiv = document.getElementById("app");
