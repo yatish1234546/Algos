ea52559d84b29bf7e0277cc9ec77654a37c30de1
yatish1234546 <katagaryatish@gmail.com>
2020-12-22 23:27:46 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import "./style.css";
 // import "./multiple-pointers/checkForSum.js";0
 // import "./Sorting/BubbleSort.js";
 // import "./Search/BinarySearch.js";
-import "./Search/NaiveStringSearch.js";
-
+// import "./Search/NaiveStringSearch.js";
+import "./recursion/power.js";
 // Write Javascript code!
 console.log("JS Loaded");
 const appDiv = document.getElementById("app");
---
+++ recursion/power.js
@@ -0,0 +1,14 @@
+console.log(power(2, 0)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(2, 4)); // 16
+
+function power(num, pow) {
+  if (num === 0) {
+    return num;
+  }
+  if (pow === 0) {
+    return 1;
+  }
+
+  return num * power(num, pow - 1);
+}
