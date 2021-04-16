3bad5f5dc8e4cfae469402cc90ea061ec2e99c64
yatish1234546 <katagaryatish@gmail.com>
2021-04-16 11:28:54 +0530

    1 file modified

--- stacks/stacksBecomeQueue.js
+++ stacks/stacksBecomeQueue.js
@@ -1,9 +1,40 @@
 import Stack from "./stack";
 
-const stackOne = new Stack();
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
 
-stackOne.add("Green");
-stackOne.add("Blue");
-stackOne.add("Red");
+  add(record) {
+    this.first.add(record);
+  }
 
-console.log(stackOne);
+  remove() {
+    while (this.first.peek()) {
+      this.second.add(this.first.remove());
+    }
+
+    const record = this.second.remove();
+
+    while (this.second.peek()) {
+      this.first.add(this.second.remove());
+    }
+
+    return record;
+  }
+}
+
+const queueFromStack = new Queue();
+const testStack = new Stack();
+
+testStack.add("Green");
+testStack.add("Blue");
+testStack.add("Red");
+
+queueFromStack.add("Green");
+queueFromStack.add("Blue");
+queueFromStack.add("Red");
+
+console.log(queueFromStack.remove());
+console.log(testStack.remove());
